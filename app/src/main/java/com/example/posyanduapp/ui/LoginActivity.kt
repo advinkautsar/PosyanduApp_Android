f2412a5c3f83670091e0b9732224bf1dc7f479af
advin kautsar <advinkautsar07@gmail.com>
2021-12-29 14:03:04 +0700

    init ( UI splashscreen & UI login )

---
+++ app/src/main/java/com/example/posyanduapp/ui/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.example.posyanduapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.posyanduapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+
+    }
+}
