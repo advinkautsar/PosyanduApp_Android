1c0019797bb1d6fde5618bc306a4b0d6437b045d
advinkautsar <advinkautsar07@gmail.com>
2022-07-17 19:28:30 +0700

    penyesuaian

--- app/src/main/java/com/example/posyanduapp/retrofit/ApiService.kt
+++ app/src/main/java/com/example/posyanduapp/retrofit/ApiService.kt
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ApiService {
 
 //    val BASE_URL: String = "http://192.168.100.9/TA_Posyandu2021/api/"
-    val BASE_URL: String = "http://192.168.1.4/PosyanduWeb/api/"
+    val BASE_URL: String = "http://192.168.100.52/SistemInformasiPosyandu/public/api/"
 
     val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
